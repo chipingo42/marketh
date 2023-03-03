1b3f3cbe4de0740ef7bd4b8233b27194076aa967
victor <82973437+chipingo42@users.noreply.github.com>
2023-03-03 11:57:42 +0100

    changes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Clients from './Components/Clients';
 import Compaines from './Components/Compaines';
 import CuttingEdge from './Components/CuttingEdge';
 import Hero from './Components/Hero';
@@ -15,6 +16,7 @@ function App() {
       <Compaines />
       <Hire />
       <CuttingEdge />
+      <Clients />
     </div>
   );
 }
